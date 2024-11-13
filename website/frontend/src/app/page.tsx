ae8d9305cc1cb499b1c981b35d41122302f5ddc2
bhavitaap <bhavita.peddi@gmail.com>
2024-11-12 21:52:58 -0800

    contact page not used

--- website/frontend/src/app/page.tsx
+++ website/frontend/src/app/page.tsx
@@ -1,9 +1,42 @@
 import Navbar from '../components/Navbar';
 import React from 'react';
+import TextField from '@mui/material/TextField';
+
 export default function Home() {
   return (
     <div>
       <Navbar />
+      
+      <div style={{padding: '20px'}}>
+        <div style={{padding: '20px'}}>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Name"
+            placeholder="Email"
+            multiline
+            maxRows={4}
+          />
+        </div>
+        <div style={{padding: '20px'}}>
+        <TextField
+          id="outlined-textarea"
+          label="Email"
+          placeholder="Email"
+          multiline
+        />
+        </div>
+        <div style={{padding: '20px'}}>
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          placeholder="Please input your message here!"
+        />
+        </div>
+      </div>
+
+
     </div>
   );
 }
